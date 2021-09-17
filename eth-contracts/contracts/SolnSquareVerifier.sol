871a1ff664175a15f696a47d2d3d94eac78248fe
danczw <dev.dc@pm.me>
2021-09-17 19:42:22 +0200

    [feature] combine token contract and zkSNARK contract, add functions

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -2,60 +2,72 @@
 
 pragma solidity ^0.8.0;
 
+import "./Verifier.sol";
+import "./ERC721Mintable.sol";
+
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
+contract SquareVerifier is Verifier { 
+    function verify(
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) public view returns (bool) {
+        return super.verifyTx(a, b, c, input);
+    }
+}
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is propertyERC721Token {
+    SquareVerifier private squareVerifier;
+    
+    constructor(address verifierAddress) propertyERC721Token(
+        "Property Title Listing", "PTL"
+    ) {
+        squareVerifier = SquareVerifier(verifierAddress);
+    }
+    // TODO define a solutions struct that can hold an index & an address
+    struct solution {
+        uint256 tokenId;
+        address to;
+    }
 
+    // TODO define an array of the above struct
+    solution[] private solutionsArr; 
 
+    // TODO define a mapping to store unique solutions submitted
+    mapping(bytes32 => solution) solutionsMap;
 
-// TODO define a solutions struct that can hold an index & an address
+    // TODO Create an event to emit when a solution is added
+    event solutionAdded(uint tokenId, address to);
 
+    // TODO Create a function to add the solutions to the array and emit the event
+    function addSolution(address _to, uint256 _tokenId, bytes32 _index) public {
+        solutionsArr.push(solution(_tokenId, _to));
+        solutionsMap[_index] = solution(_tokenId, _to);
+        emit solutionAdded(_tokenId, _to);
+    }
 
-// TODO define an array of the above struct
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSupply
+    function mintToken(
+        address _to,
+        uint _tokenId,
+        uint[2] memory a,
+        uint[2][2] memory b,
+        uint[2] memory c,
+        uint[2] memory input
+    ) public returns (bool) {
+        
+        bytes32 _inputHash = keccak256(abi.encodePacked(a,b,c,input));
+        require(solutionsMap[_inputHash].to == address(0), "Solution already in use.");
+        require(squareVerifier.verify(a, b, c, input), "Proof is invalid");
 
+        addSolution(_to, _tokenId, _inputHash);
+        super.mintEnumeration(_to, _tokenId);
+        super.setTokenURI(_tokenId);
 
-// TODO define a mapping to store unique solutions submitted
-
-
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return true;
+    }
+}
